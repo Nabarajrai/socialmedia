253e78ecd78c5efe669ff3865b082dcf93ecf404
nabaraj <salapanabaraj2023@gmail.com>
2024-06-16 15:04:00 +0545

    created righ side bar

--- src/pages/home/Home.pages.jsx
+++ src/pages/home/Home.pages.jsx
@@ -1,14 +1,17 @@
 import LayoutComponent from "../../layout/Layout.component";
 import "../../styles/pages/_home.scss";
+import SideNavbar from "../../components/sidenavbar/SideNavbar";
 const HomePage = () => {
   return (
+    <div className="home-page-setion">
     <LayoutComponent>
       <div className="home-page">
-        <div className="sidebar-left">left side bar</div>
+        <div className="sidebar-left"><SideNavbar/></div>
         <div className="main-content">main</div>
         <div className="sidebar-right">righ bar</div>
       </div>
     </LayoutComponent>
+    </div>
   );
 };
 
